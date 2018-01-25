6fee06ab36b3d766275510398443e47797a5c78b
aymen <aymen.sellaouti@gmail.com>
2018-01-25 09:00:06 +0100

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
+import { Compo2Component } from './compo2.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { BindingComponent } from './binding/binding.component';
+import { DivcolorComponent } from './divcolor/divcolor.component';
+import { TwowayComponent } from './twoway/twoway.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Compo1Component,
+    Compo2Component,
+    InterpolationComponent,
+    BindingComponent,
+    DivcolorComponent,
+    TwowayComponent
   ],
   imports: [
     BrowserModule,
